9221425d5177c2fc82c9df687de01f13923cf9df
runinbk <runinbk@gmail.com>
2024-11-19 12:36:07 -0400

    Create User Model

---
+++ src/modules/auth/models/User.js
@@ -0,0 +1,53 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../../../config/database');
+const bcrypt = require('bcryptjs');
+
+const User = sequelize.define('User', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  username: {
+    type: DataTypes.STRING(50),
+    unique: true,
+    allowNull: false
+  },
+  email: {
+    type: DataTypes.STRING(100),
+    unique: true,
+    allowNull: false,
+    validate: {
+      isEmail: true
+    }
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  role: {
+    type: DataTypes.ENUM('admin', 'doctor', 'nurse', 'staff'),
+    defaultValue: 'staff'
+  },
+  isActive: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: true
+  },
+  lastLogin: {
+    type: DataTypes.DATE
+  }
+}, {
+  hooks: {
+    beforeSave: async (user) => {
+      if (user.changed('password')) {
+        user.password = await bcrypt.hash(user.password, 10);
+      }
+    }
+  }
+});
+
+User.prototype.validatePassword = async function(password) {
+  return bcrypt.compare(password, this.password);
+};
+
+module.exports = User;
